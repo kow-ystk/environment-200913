0b8981d3e8649f98ddc82c66531f302fdf5f2306
Koki Kamezono <kow1ag25@gmail.com>
2020-09-13 18:35:29 +0900

    add: watchタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,27 @@ const gulp = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
 
-// style.scssをタスクを作成する
+// style.scssの監視タスクを作成する
 gulp.task("default", function () {
-  // style.scssファイルを取得
-  return (
-    gulp
-      .src("_dev/scss/style.scss")
-      // Sassのコンパイルを実行
-      .pipe(
-        sass({
-          outputStyle: "expanded",
-        })
-      )
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("dest/css"))
-  );
+  // style.scssファイルを監視
+  return gulp.watch("_dev/scss/style.scss", function () {
+    // style.scssの更新があった場合の処理
+
+    // style.scssファイルを取得
+    return (
+      gulp
+        .src("_dev/scss/style.scss")
+        // Sassのコンパイルを実行
+        .pipe(
+          sass({
+            outputStyle: "expanded",
+          })
+            // Sassのコンパイルエラーを表示
+            // (これがないと自動的に止まってしまう)
+            .on("error", sass.logError)
+        )
+        // cssフォルダー以下に保存
+        .pipe(gulp.dest("dest/css"))
+    );
+  });
 });
